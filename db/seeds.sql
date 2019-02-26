78d044a0b22526a3b3d51efa279af86b3d5228db
Artemis <35303907+slane1980@users.noreply.github.com>
2019-02-25 21:57:55 -0700

    added schema and seeds

---
+++ db/seeds.sql
@@ -0,0 +1,12 @@
+INSERT INTO burgers
+    (burger_name, devoured)
+VALUES
+    ('Veggie Burger', FALSE);
+INSERT INTO burgers
+    (burger_name, devoured)
+VALUES
+    ('Cheese Burger', FALSE);
+INSERT INTO burgers
+    (burger_name, devoured)
+VALUES
+    ('Bacon Burger', FALSE);
